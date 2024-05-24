0182444d1bd97fb753f9a552c96155dcbb79d3db
Jamesogood <carrascalmarissa5@gmail.com>
2024-05-24 23:24:52 +0800

    Add project files.

---
+++ EmployeeApplication/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace EmployeeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.fName = new System.Windows.Forms.TextBox();
+            this.lName = new System.Windows.Forms.TextBox();
+            this.dept = new System.Windows.Forms.TextBox();
+            this.jobTitle = new System.Windows.Forms.TextBox();
+            this.ratePerHour = new System.Windows.Forms.TextBox();
+            this.totalHoursWorked = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.firstname = new System.Windows.Forms.Label();
+            this.lastname = new System.Windows.Forms.Label();
+            this.salary = new System.Windows.Forms.Label();
+            this.computeSalary = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(233, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Department";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(233, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Job Title";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Rate per hour";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(233, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 21);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Total hours worked";
+            // 
+            // fName
+            // 
+            this.fName.BackColor = System.Drawing.Color.Khaki;
+            this.fName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fName.Location = new System.Drawing.Point(16, 65);
+            this.fName.Name = "fName";
+            this.fName.Size = new System.Drawing.Size(194, 26);
+            this.fName.TabIndex = 6;
+            // 
+            // lName
+            // 
+            this.lName.BackColor = System.Drawing.Color.Khaki;
+            this.lName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lName.Location = new System.Drawing.Point(237, 65);
+            this.lName.Name = "lName";
+            this.lName.Size = new System.Drawing.Size(194, 26);
+            this.lName.TabIndex = 7;
+            // 
+            // dept
+            // 
+            this.dept.BackColor = System.Drawing.Color.Khaki;
+            this.dept.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dept.Location = new System.Drawing.Point(16, 150);
+            this.dept.Name = "dept";
+            this.dept.Size = new System.Drawing.Size(194, 26);
+            this.dept.TabIndex = 8;
+            // 
+            // jobTitle
+            // 
+            this.jobTitle.BackColor = System.Drawing.Color.Khaki;
+            this.jobTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jobTitle.Location = new System.Drawing.Point(237, 150);
+            this.jobTitle.Name = "jobTitle";
+            this.jobTitle.Size = new System.Drawing.Size(194, 26);
+            this.jobTitle.TabIndex = 9;
+            // 
+            // ratePerHour
+            // 
+            this.ratePerHour.BackColor = System.Drawing.Color.Khaki;
+            this.ratePerHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ratePerHour.Location = new System.Drawing.Point(16, 242);
+            this.ratePerHour.Name = "ratePerHour";
+            this.ratePerHour.Size = new System.Drawing.Size(194, 26);
+            this.ratePerHour.TabIndex = 10;
+            // 
+            // totalHoursWorked
+            // 
+            this.totalHoursWorked.BackColor = System.Drawing.Color.Khaki;
+            this.totalHoursWorked.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalHoursWorked.Location = new System.Drawing.Point(237, 242);
+            this.totalHoursWorked.Name = "totalHoursWorked";
+            this.totalHoursWorked.Size = new System.Drawing.Size(194, 26);
+            this.totalHoursWorked.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(13, 322);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "First name:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(13, 356);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Last name:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(13, 387);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 16);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Basic salary:";
+            // 
+            // firstname
+            // 
+            this.firstname.AutoSize = true;
+            this.firstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstname.Location = new System.Drawing.Point(91, 322);
+            this.firstname.Name = "firstname";
+            this.firstname.Size = new System.Drawing.Size(108, 16);
+            this.firstname.TabIndex = 15;
+            this.firstname.Text = "<first name here>";
+            // 
+            // lastname
+            // 
+            this.lastname.AutoSize = true;
+            this.lastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastname.Location = new System.Drawing.Point(91, 356);
+            this.lastname.Name = "lastname";
+            this.lastname.Size = new System.Drawing.Size(109, 16);
+            this.lastname.TabIndex = 16;
+            this.lastname.Text = "<last name here>";
+            // 
+            // salary
+            // 
+            this.salary.AutoSize = true;
+            this.salary.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salary.Location = new System.Drawing.Point(103, 387);
+            this.salary.Name = "salary";
+            this.salary.Size = new System.Drawing.Size(38, 16);
+            this.salary.TabIndex = 17;
+            this.salary.Text = "00.00";
+            // 
+            // computeSalary
+            // 
+            this.computeSalary.BackColor = System.Drawing.Color.Gold;
+            this.computeSalary.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computeSalary.Location = new System.Drawing.Point(145, 272);
+            this.computeSalary.Name = "computeSalary";
+            this.computeSalary.Size = new System.Drawing.Size(155, 35);
+            this.computeSalary.TabIndex = 18;
+            this.computeSalary.Text = "Compute Salary";
+            this.computeSalary.UseVisualStyleBackColor = false;
+            this.computeSalary.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(476, 450);
+            this.Controls.Add(this.computeSalary);
+            this.Controls.Add(this.salary);
+            this.Controls.Add(this.lastname);
+            this.Controls.Add(this.firstname);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.totalHoursWorked);
+            this.Controls.Add(this.ratePerHour);
+            this.Controls.Add(this.jobTitle);
+            this.Controls.Add(this.dept);
+            this.Controls.Add(this.lName);
+            this.Controls.Add(this.fName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox fName;
+        private System.Windows.Forms.TextBox lName;
+        private System.Windows.Forms.TextBox dept;
+        private System.Windows.Forms.TextBox jobTitle;
+        private System.Windows.Forms.TextBox ratePerHour;
+        private System.Windows.Forms.TextBox totalHoursWorked;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label firstname;
+        private System.Windows.Forms.Label lastname;
+        private System.Windows.Forms.Label salary;
+        private System.Windows.Forms.Button computeSalary;
+    }
+}
+
---
+++ EmployeeApplication/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PartTimeEmployee partTimeEmployee = new PartTimeEmployee(fName.Text, lName.Text, dept.Text, jobTitle.Text);
+
+            double hour = Convert.ToDouble(ratePerHour.Text);
+            int totalHour = Convert.ToInt32(totalHoursWorked.Text);
+            partTimeEmployee.computeSalary(totalHour, hour);
+
+            firstname.Text = fName.Text;
+            lastname.Text = lName.Text;
+            salary.Text = Convert.ToString(partTimeEmployee.getSalary());
+        }
+    }
+}
